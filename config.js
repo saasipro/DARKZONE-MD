a7ab9ff91505b87a0b26e3a592c0e855740fbab0
saasipro <saasifypro@gmail.com>
2025-06-05 19:53:55 +0500

    Update config.js

--- config.js
+++ config.js
@@ -8,4 +8,6 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG : process.env.ALIVE_IMG || "https://files.catbox.moe/v40suc",
 ALIVE_SMG : process.env.ALIVE_SMG ||  "HY , IM MALIK BOT",
+AUTO_READ_STATUS : process.env.AUTO_READ_STATUS || "true",
+    
 };
