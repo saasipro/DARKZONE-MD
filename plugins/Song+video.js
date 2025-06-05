1b41430b3720db3ed89cb4f48be93622363b9cc8
saasipro <saasifypro@gmail.com>
2025-06-05 23:00:45 +0500

    Update Song+video.js

--- plugins/Song+video.js
+++ plugins/Song+video.js
@@ -1,1 +1,40 @@
+const {cmd , commands} = require('../command')
+const fg = require(`api-dylux`)
+const yts = require(`yt-search`)
+cmd({
+    pattern: "song",
+    desc: "download songs ",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("please give me url or tittle")
+const search = await yts(q)
+const data = search.videos[0];
+const url = data.url 
+let desc = `
+YOURBIT SONG DOWNLOADEDER 🔥
 
+tittle: ${data.tittle}
+description: ${data.description}
+time: ${data.timestamp}
+ago: ${data.ago}
+views: ${data.views}
+made by irfan
+`
+await conn.sendmessage(from,{image:{url:data.thumbnail}, caption:desc},{qouted:mek});
+//download audio 
+let down = await fg.yta(url)
+let downloadurl = down.dl_url
+  
+ //send audio message 
+await conn.sendmessage(from,{audio:{url:downloadurl},mimetype:"audio/mpag" },{qouted:mek})
+
+
+  
+}catch(e){
+console.log(e)
+reply('${e}')
+}
+})
