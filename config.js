2e4cb71eaeed7f25f8a13cbd666b2e11a42fdfd3
saasipro <saasifypro@gmail.com>
2025-06-07 11:14:04 +0500

    Update config.js

--- config.js
+++ config.js
@@ -9,5 +9,5 @@ SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG : process.env.ALIVE_IMG || "https://files.catbox.moe/v40suc",
 ALIVE_SMG : process.env.ALIVE_SMG ||  "HY , IM MALIK BOT",
 AUTO_READ_STATUS : process.env.AUTO_READ_STATUS || "true",
-    
+MODE : process.env.MODE || "public",   
 };
