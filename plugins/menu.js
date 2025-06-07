88acbb8a3d8370bb0a529bacc07b7c6e64f428b1
saasipro <saasifypro@gmail.com>
2025-06-07 19:26:59 +0500

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,4 +1,4 @@
-const config = require('../config')
+const {readEnv} = require('../lib/database')
 const {cmd , commands} = require('../command')
 
 cmd({
@@ -9,6 +9,7 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
+const config = await readEnv();
 let menu = {
 main: '',
 download: '',
@@ -20,7 +21,7 @@ search: '',
 
 ‎for (let i=0; i < commands.length; i++) {
 ‎if (commands[i].pattern && !commands[i]. dontAddCommandlist){
-menu [commands[i].category] += `.${commands[i].pattern}\n` 
+menu [commands[i].category] += `${config.PREFIX}${commands[i].pattern}\n` 
 }   
 }
 let madeMemu = `👋*hello ${pushname}*
@@ -50,7 +51,7 @@ ${menu.search}
 
 POWERD BY IRFAN 💜
 `
-await conn.SendMessage(from,{image:{url:"https://files.catbox.moe/gs054b"},caption:madeMenu},{quoted:mek})
+await conn.SendMessage(from,{image:{url:config.ALIVE_IMG"},caption:madeMenu},{quoted:mek})
 
 }catch(e){
 console.log(e)
