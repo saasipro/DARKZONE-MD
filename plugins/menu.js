e1d3fde3f4f994a1a313a1b3587a3ec0dc611596
saasipro <saasifypro@gmail.com>
2025-06-07 13:41:59 +0500

    Create menu.js

---
+++ plugins/menu.js
@@ -0,0 +1,26 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+
+cmd({
+    pattern: "menu",
+    desc: "get cmd list",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let menu = {
+main: '',
+download: '',
+group: '',
+owner: '',
+convert: '',
+search: '',
+}; 
+}
+
+}catch(e){
+console.log(e)
+reply(`${e}`) 
+}
+  
